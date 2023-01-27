6cbaf46ae8bae8ae0bab8e834e106ac09ffe9b23
FranDavB <francodbaretta@gmail.com>
2023-01-27 11:43:18 -0300

    asd

--- argentina-programa-frontend-dinamico/src/app/app.module.ts
+++ argentina-programa-frontend-dinamico/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavegacionBarraComponent } from './componentes/navegacion-barra/navegacion-barra.component';
 import { BotonLoginComponent } from './componentes/boton-login/boton-login.component';
+import { FormularioLoginComponent } from './componentes/formulario-login/formulario-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavegacionBarraComponent,
-    BotonLoginComponent
+    BotonLoginComponent,
+    FormularioLoginComponent
   ],
   imports: [
     BrowserModule,
